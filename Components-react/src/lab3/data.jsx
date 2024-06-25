7a948531b9fae088376993eebb30d850bb045d73
ViNx3h <vinhphuochongnnn@gmail.com>
2024-06-25 22:31:05 +0700

    Add files via upload

---
+++ Components-react/src/lab3/data.jsx
@@ -0,0 +1,40 @@
+export const data = [
+    {
+        question: "What is ReactJS?",
+        option1: "A JavaScript library for building user interfaces",
+        option2: "A programming language",
+        option3: "A database management system",
+        ans: 1,
+    },
+    {
+        question: "What is JSX",
+        option1: "A programming language",
+        option2: "A file format",
+        option3: "A syntax extension for JavaScript",
+        ans: 3,
+    },
+    {
+        question: "Which continent has the highest number of countries?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question: "Which of the following are types of computer hardware?",
+        option1: "RAM",
+        option2: "FireFox",
+        option3: "Microsoft Windows",
+        option4: "Excel",
+        ans: 1,
+    },
+    {
+        question: "What is Wi-Fi?",
+        option1: "A type of sound card",
+        option2: "A type of wireless network",
+        option3: "An extra-wide computer case used",
+        option4: "A type of software that scans for viruses",
+        ans: 2,
+    },
+];
